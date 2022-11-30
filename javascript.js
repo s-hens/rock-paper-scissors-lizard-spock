d08c9061a97d17aea1fa51a2dcf16db1c5c220cc
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-11-30 20:19:31 +0000

    Add explanatory comment for personal reference

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
-const options = ["rock", "paper", "scissors", "lizard", "spock"];
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 function getComputerChoice() {
-    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    let computerChoice = choices[Math.floor(Math.random() * 5)];
     console.log `${computerChoice}`;
 }
 
 getComputerChoice();
 
+/*
+[Math.random() * (max-min) + min] returns a random number < max and >= min.
+In the array "choices" there are 5 items, [0] [1] [2] [3] [4].
+So I use [(Math.random() * (5-0) + 0)] simplified as [(Math.random() * 5)]
+to return a number < 5 and >= 0.
+
+Math.floor() rounds down to the nearest integer,
+thus the function returns an integer between 0 and 4.
+
+Could also use choices.length instead of 5.
+*/
+
 function getPlayerChoice() {
-    const playerChoice = prompt(`Choose your weapon`, ``);
-    const playerChoiceLowerCase = playerChoice.toLowerCase();
-    console.log `${playerChoiceLowerCase}`;
+    let playerChoice = prompt(`Choose your weapon`, ``);
+    playerChoice = playerChoice.toLowerCase();
+    console.log `${playerChoice}`;
 }
 
 getPlayerChoice();
+
+function matchUp(computerChoice, playerChoice) {
+
+}
