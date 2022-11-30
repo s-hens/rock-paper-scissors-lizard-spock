61a1b17763c74414b19323ded812d89af818fc4f
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-11-30 16:17:23 +0000

    Create function for computer to choose random option

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const options = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+function getComputerChoice() {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    console.log `${computerChoice}`;
+}
+
+getComputerChoice();
