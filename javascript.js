1cb4e3d115835573932d35c293ae8cba802d61d0
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-12-05 11:18:50 +0000

    Defer JS

--- javascript.js
+++ javascript.js
@@ -17,10 +17,8 @@ Line 11:
 [Math.random() * (max-min) + min] returns a random number < max and >= min.
 In the array "choices" there are 5 items, [0] [1] [2] [3] [4].
 So I use [(Math.random() * (5-0) + 0)] simplified as [(Math.random() * 5)] to return a number < 5 and >= 0.
-
 Math.floor() rounds down to the nearest integer,
 thus the function returns an integer between 0 and 4.
-
 Could also use choices.length instead of 5.
 */
 
